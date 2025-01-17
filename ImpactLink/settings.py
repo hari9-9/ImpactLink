946502c501b89eedfcc7508b1387ef138150ec3f
Hari <reachk.hariharan@gmail.com>
2025-01-17 01:11:14 +0000

    integrated postgres

--- ImpactLink/settings.py
+++ ImpactLink/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'ImpactLink.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'impacturl',
+        'USER': 'tester',
+        'PASSWORD': 'test123',
+        'HOST': 'localhost',  # or your database host
+        'PORT': '5432',       # default PostgreSQL port
     }
 }
 
